a31a0588a80fe96ba747d545c77b1956cdc15d58
dthomas7 <danathomas@quickenloans.com>
2016-07-05 18:11:04 -0400

    filters

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ $.ajax({
 	dataType: "json",
 	success: function(response) {
 		$.each(response.apartments, function(i, apartment) {
-			var listing = "<a href='#' class='list-group-item listings'><h4 class='list-group-item-heading'>" + apartment.description + " BR / " + apartment.bedrooms + " / " + apartment.price + "</h4><p class='list-group-item-text'>" + apartment.neighborhood + "</p></a>"
+			var apartmentClass = apartment.city.toLowerCase().replace(" ", "-")
+			var listing = "<a href='#' class='list-group-item " + apartmentClass + " listings'><h4 class='list-group-item-heading'>" + apartment.description + " BR / " + apartment.bedrooms + " / " + apartment.price + "</h4><p class='list-group-item-text'>" + apartment.neighborhood + "</p></a>"
 			$(".apartments").append(listing);
 		});
 	},
@@ -14,11 +15,16 @@ $.ajax({
 });
 
 $(".filter").click(function() {
-	var city = "$(this).attr("id");
+	$(".filter").removeClass("active");
+	$(this).addClass("active");
+	$(".listings").show();
 
-	$(".listings").not()
-
+	var city = $(this).attr("id");
+	if (city !== "all") {
+		$(".listings").not("." + city).css("display", "none");
+	}
 });
+
 
 
 
