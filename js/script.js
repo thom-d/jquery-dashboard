a74b161e581f12b601dc0eef48756ce01740c8a7
dthomas7 <danathomas@quickenloans.com>
2016-07-06 08:57:54 -0400

    completed app code

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $.ajax({
 	success: function(response) {
 		$.each(response.apartments, function(i, apartment) {
 			var apartmentClass = apartment.city.toLowerCase().replace(" ", "-")
-			var listing = "<a href='#' class='list-group-item " + apartmentClass + " listings'><h4 class='list-group-item-heading'>" + apartment.description + " BR / " + apartment.bedrooms + " / " + apartment.price + "</h4><p class='list-group-item-text'>" + apartment.neighborhood + "</p></a>"
+			var listing = "<a href='#' id=" + apartment.id + " class='list-group-item " + apartmentClass + " listings'><h4 class='list-group-item-heading'>" + apartment.description + " BR / " + apartment.bedrooms + " / " + apartment.price + "</h4><p class='list-group-item-text'>" + apartment.neighborhood + "</p></a>"
 			$(".apartments").append(listing);
 		});
 	},
@@ -23,6 +23,28 @@ $(".filter").click(function() {
 	if (city !== "all") {
 		$(".listings").not("." + city).css("display", "none");
 	}
+});
+
+$(document).on("click", ".listings", function() {
+var id = $(this).attr("id");
+
+	$.ajax({
+	url: "https://api.myjson.com/bins/2sadq?pretty=1",
+	dataType: "json",
+	success: function(response) {
+		var selectedApartment = $.grep(response.apartments, function(apartment) {
+			return apartment.id == id;
+		})
+		var address = selectedApartment[0].address;
+
+		window.open("http://maps.google.com/?q=" + address);
+	},
+	error: function(error) {
+		console.log(error);
+	}
+
+});
+
 });
 
 
